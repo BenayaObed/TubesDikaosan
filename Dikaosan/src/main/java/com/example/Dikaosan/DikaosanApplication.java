6eb4a59277b52e0999632cee96a2e9e62462a97b
BenayaObed <91049664+BenayaObed@users.noreply.github.com>
2023-10-10 18:06:32 +0700

    memasukan spring

---
+++ Dikaosan/src/main/java/com/example/Dikaosan/DikaosanApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dikaosan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DikaosanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DikaosanApplication.class, args);
+	}
+
+}
---
+++ Dikaosan/src/test/java/com/example/Dikaosan/DikaosanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dikaosan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DikaosanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
