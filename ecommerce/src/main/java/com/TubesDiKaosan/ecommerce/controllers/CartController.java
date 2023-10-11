049583d6d2b95df6ffd79b7a4f82138cfd1bd396
irizkyw <ichwanrizkywahyudin3@gmail.com>
2023-10-11 18:28:32 +0700

    Layouting and support jpa ready for frontend and backend

--- ecommerce/src/main/java/com/TubesDiKaosan/ecommerce/controllers/CartController.java
+++
@@ -1,5 +0,0 @@
-package com.TubesDiKaosan.ecommerce.controllers;
-
-public class CartController {
-    
-}
---
+++ src/main/java/com/TubesDiKaosan/ecommerce/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.TubesDiKaosan.ecommerce.controllers;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class HomeController{
+    @RequestMapping("/")
+    public String homePage() {
+        return "home";
+    }
+}
