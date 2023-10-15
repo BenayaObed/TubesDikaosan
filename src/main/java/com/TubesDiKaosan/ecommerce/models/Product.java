8b5a06046c761772f7a170ce4a1117a12aee4a26
irizkyw <ichwanrizkywahyudin3@gmail.com>
2023-10-15 08:27:30 +0700

    fixed model Product

--- src/main/java/com/TubesDiKaosan/ecommerce/models/Product.java
+++ src/main/java/com/TubesDiKaosan/ecommerce/models/Product.java
@@ -1,28 +1,49 @@
 package com.TubesDiKaosan.ecommerce.models;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Product")
-
 public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private int product_id;
+  @Column(length = 100)
   private String name_product;
-  
+
   @ManyToOne
   @JoinColumn(name = "category_id", referencedColumnName = "category_id")
   private Category category;
+
+  // TEXT
+  @Column(columnDefinition = "TEXT")
   private String description;
+  @Column(length = 255)
   private String image;
+
   private int price;
   private int stock;
-  
-  public Product() {}
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
   public Product(int product_id, String name_product, Category category, String description, String image, int price,
-      int stock) {
+    int stock, LocalDateTime createdAt, LocalDateTime updatedAt) {
     this.product_id = product_id;
     this.name_product = name_product;
     this.category = category;
@@ -30,48 +51,82 @@ public class Product {
     this.image = image;
     this.price = price;
     this.stock = stock;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
   }
+
+  public Product() {
+  }
+
   public int getProduct_id() {
     return product_id;
   }
+
   public void setProduct_id(int product_id) {
     this.product_id = product_id;
   }
+
   public String getName_product() {
     return name_product;
   }
+
   public void setName_product(String name_product) {
     this.name_product = name_product;
   }
+
   public Category getCategory() {
     return category;
   }
+
   public void setCategory(Category category) {
     this.category = category;
   }
+
   public String getDescription() {
     return description;
   }
+
   public void setDescription(String description) {
     this.description = description;
   }
+
   public String getImage() {
     return image;
   }
+
   public void setImage(String image) {
     this.image = image;
   }
+
   public int getPrice() {
     return price;
   }
+
   public void setPrice(int price) {
     this.price = price;
   }
+
   public int getStock() {
     return stock;
   }
+
   public void setStock(int stock) {
     this.stock = stock;
   }
 
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
 }
