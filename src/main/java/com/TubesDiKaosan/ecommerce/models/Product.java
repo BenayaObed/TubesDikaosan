aaaf39dfc69abe7898a05fd5ae400360d17699e8
irizkyw <ichwanrizkywahyudin3@gmail.com>
2023-10-16 13:38:36 +0700

    fixed model Product

--- src/main/java/com/TubesDiKaosan/ecommerce/models/Product.java
+++ src/main/java/com/TubesDiKaosan/ecommerce/models/Product.java
@@ -50,13 +50,10 @@ public class Product {
   @Column(name = "updatedAt")
   private LocalDateTime updatedAt;
 
-  public Product() {
-  }
-
-  public Product(int product_id, String product_name, Category category, String description, List<Images> images,
+  public Product(int product_id, String name_product, Category category, String description, List<Images> images,
       int price, List<Stock> stock, LocalDateTime createdAt, LocalDateTime updatedAt) {
     this.product_id = product_id;
-    this.product_name = product_name;
+    this.name_product = name_product;
     this.category = category;
     this.description = description;
     this.images = images;
@@ -66,37 +63,49 @@ public class Product {
     this.updatedAt = updatedAt;
   }
 
-  public Product() {
-
-  }
-
   public int getProduct_id() {
     return product_id;
   }
 
-  public String getProduct_name() {
-    return product_name;
+  public void setProduct_id(int product_id) {
+    this.product_id = product_id;
+  }
+
+  public String getName_product() {
+    return name_product;
+  }
+
+  public void setName_product(String name_product) {
+    this.name_product = name_product;
   }
 
   public Category getCategory() {
     return category;
   }
 
+  public void setCategory(Category category) {
+    this.category = category;
+  }
+
   public String getDescription() {
     return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
   }
 
   public List<Images> getImages() {
     return images;
   }
 
+  public void setImages(List<Images> images) {
+    this.images = images;
+  }
+
   public int getPrice() {
     return price;
   }
-
-  <<<<<<<
-
-  Updated upstream
 
   public void setPrice(int price) {
     this.price = price;
@@ -110,63 +119,20 @@ public class Product {
     this.stock = stock;
   }
 
-  =======
-
-  public int getStock() {
-    return stock;
-  }
-
-  >>>>>>>
-
-  Stashed changes
-
   public LocalDateTime getCreatedAt() {
     return createdAt;
-  }
-
-  public LocalDateTime getUpdatedAt() {
-    return updatedAt;
-  }
-
-  public void setProduct_id(int product_id) {
-    this.product_id = product_id;
-  }
-
-  public void setProduct_name(String product_name) {
-    this.product_name = product_name;
-  }
-
-  public void setCategory(Category category) {
-    this.category = category;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public void setImages(List<Images> images) {
-    this.images = images;
-  }
-
-  public void setPrice(int price) {
-    this.price = price;
-  }
-
-  public void setStock(int stock) {
-    this.stock = stock;
   }
 
   public void setCreatedAt(LocalDateTime createdAt) {
     this.createdAt = createdAt;
   }
 
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+
   public void setUpdatedAt(LocalDateTime updatedAt) {
     this.updatedAt = updatedAt;
   }
 
-  <<<<<<<
-
-  Updated upstream}=======
-}>>>>>>>
-Stashed
-changes
+}
